59170717dd39541852b80e4175d417e40a60b88c
Fernando Robledo <fernando.robledoruiz@gmail.com>
2024-02-14 08:17:14 -0500

    Using OpenAI Functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
-from langchain_openai.llms import OpenAI
+from langchain_openai import ChatOpenAI, OpenAI
 from dotenv import load_dotenv
 
 
@@ -16,9 +17,13 @@ def main():
     if user_csv is not None:
         user_question = st.text_input("Ask your question about your CSV file")
 
-        llm = OpenAI(temperature=0)
+        # llm = OpenAI(temperature=0)
+        llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
 
-        agent = create_csv_agent(llm, user_csv, verbose=True)
+        # agent = create_csv_agent(
+        #     llm, user_csv, verbose=True, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
+        agent = create_csv_agent(
+            llm, user_csv, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
 
         if user_question is not None and user_question != "":
             response = agent.run(user_question)
